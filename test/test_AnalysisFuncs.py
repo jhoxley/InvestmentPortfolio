0d4b321555ed2ef7ebf44cb704291d30bf51a3dd
Jack Hoxley <jhoxley@hotmail.co.uk>
2025-08-18 16:34:09 +0100

    Refactored analysis func unit tests; basic non-working framework for 1st unit test needed.

--- test/test_AnalysisFuncs.py
+++ test/test_AnalysisFuncs.py
@@ -4,50 +4,53 @@ import AnalysisFuncs as af
 
 class TestCases(unittest.TestCase):
     
-    def test_CalculateWeights_Simple(self):
-        # Sample DataFrame
-        summary_data = {
-            'Settle Date' : ['2023-01-01', '2023-01-02', '2023-01-03'],
-            'Total_Market_Value': [600, 610, 620]
-        }
-        summary_frame = pd.DataFrame(summary_data)
+    ### ================================
+    ### Tests for create_daily_summary()
+    ### ================================
+    def utility_create_daily_summary(self, testNum, rootpath):
+        # Load the test data
+        df = pd.read_csv(f"{rootpath}portfolio_{testNum}.csv")
 
-        position_date = {
-            'Settle Date': ['2023-01-01', '2023-01-01', '2023-01-01',
-                            '2023-01-02', '2023-01-02', '2023-01-02',
-                            '2023-01-03', '2023-01-03', '2023-01-03'
-                            ],
-            'Position Name':[ 'A', 'B', 'C',
-                              'A', 'B', 'C',
-                               'A', 'B', 'C'
-                             ],
-            'Book Market Value': [100, 200, 300,
-                                  105, 195, 310,
-                                  110, 190, 320],
-        }
-        position_frame = pd.DataFrame(position_date)
+        # Ensure the columns are in the correct format
+        df['Settle date'] = pd.to_datetime(df['Settle date'])
+        df['Book cost'] = pd.to_numeric(df['Book cost'], errors='coerce').astype('float')
+        df['Market value'] = pd.to_numeric(df['Market value'], errors='coerce').astype('float')
+        df['ITD PnL'] = pd.to_numeric(df['ITD PnL'], errors='coerce').astype('float')
 
-        # Calculate weights
-        result_df = af.calculate_weights(position_frame, summary_frame)
+        # load the expected results
+        expected_df = pd.read_csv(f"{rootpath}expected_summary_{testNum}.csv")
 
-        # Expected result
-        expected_data = {
-            'Settle Date': ['2023-01-01', '2023-01-01', '2023-01-01',
-                            '2023-01-02', '2023-01-02', '2023-01-02',
-                            '2023-01-03', '2023-01-03', '2023-01-03'
-                            ],
-            'Position Name':[ 'A', 'B', 'C',
-                              'A', 'B', 'C',
-                               'A', 'B', 'C'
-                             ],
-            'Book Market Value': [100, 200, 300,
-                                  105, 195, 310,
-                                  110, 190, 320],
-            'Portfolio Weight %' : [100.0 / 600.0, 200.0 / 600.0, 300.0 / 600.0,
-                                    105.0 / 610.0, 195.0 / 610.0, 310.0 / 610.0,
-                                    110.0 / 620.0, 190.0 / 620.0, 320.0 / 620.0]
-        }
-        expected_df = pd.DataFrame(expected_data)
+        # Ensure the expected DataFrame is in the correct format
+        expected_df['Settle date'] = pd.to_datetime(expected_df['Settle date'])
+        expected_df['Total Book Cost'] = pd.to_numeric(expected_df['Total Book Cost'], errors='coerce').astype('float')
+        expected_df['Total Market Value'] = pd.to_numeric(expected_df['Total Market Value'], errors='coerce').astype('float')
+        expected_df['Total PnL'] = pd.to_numeric(expected_df['Total PnL'], errors='coerce').astype('float')
+        expected_df['Total Return %'] = pd.to_numeric(expected_df['Total Return %'], errors='coerce').astype('float')
+        expected_df['Daily Return %'] = pd.to_numeric(expected_df['Daily Return %'], errors='coerce').astype('float')
+
+        # Call the function
+        result_df = af.create_daily_summary(df)
 
         # Check if the result matches the expected DataFrame
-        pd.testing.assert_frame_equal(result_df.round(4), expected_df.round(4))
+        pd.testing.assert_frame_equal(result_df, expected_df)
+
+    # Test 1: trivial
+    # Test 2: multiple positions, exist for all dates
+    # Test 3: multiple positions, buys and sells mid-period
+    # Test 4: multiple positions, matching sells closes position and re-opened later
+    def test_create_daily_summary_1(self):
+        # define the root path for test data
+        rootpath = "./test_data/create_daily_summary/"
+
+        # Call the utility function with test number 1
+        self.utility_create_daily_summary(1, rootpath)
+
+    ### =============================
+    ### Tests for calculate_weights()
+    ### =============================
+
+    ### =============================
+    ### Tests for calculate_returns()
+    ### =============================
+
+        
